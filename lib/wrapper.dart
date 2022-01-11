edd474d98ad3c2f308a6e348f48dbfbf1395d368
cansuengineer <cansu.uzunsimsek.1@gmail.com>
2022-01-11 14:26:38 +0300

    provider eklendi , ios auth eklendi

---
+++ lib/wrapper.dart
@@ -0,0 +1,30 @@
+import 'package:auth_provider/screens/login_screen.dart';
+import 'package:auth_provider/screens/profile_screen.dart';
+import 'package:auth_provider/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'model/user_model.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final authService = Provider.of<Auth>(context);
+    return StreamBuilder<User?>(
+        stream: authService.user,
+        builder: (_, AsyncSnapshot<User?> snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            final User? user = snapshot.data;
+            return user == null ? const LoginScreen() : const ProfileScreen();
+          } else {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
+  }
+}
